b219d9976f234928e01e50ef52649389f32a42d4
giriswaraj05-hash <giriswaraj05@gmail.com>
2026-06-27 19:30:14 +0530

    Create q_data_acn.js

---
+++ q_data_acn.js
@@ -0,0 +1,13 @@
+window.QUIZ_DATA = window.QUIZ_DATA || {};
+window.QUIZ_DATA.acn = [
+  { q: "The OSI model has how many layers?", options: ["5","6","7","8"], answer: 2 },
+  { q: "Which OSI layer is responsible for routing?", options: ["Data Link layer","Network layer","Session layer","Presentation layer"], answer: 1 },
+  { q: "An IPv4 address is how many bits long?", options: ["16 bits","32 bits","64 bits","128 bits"], answer: 1 },
+  { q: "An IPv6 address is how many bits long?", options: ["32 bits","64 bits","128 bits","256 bits"], answer: 2 },
+  { q: "Which protocol resolves an IP address to a MAC address?", options: ["ARP","DNS","FTP","DHCP"], answer: 0 },
+  { q: "TCP is a:", options: ["Connectionless protocol","Connection-oriented protocol","Both of these","None of these"], answer: 1 },
+  { q: "Which of the following is a link-state routing protocol?", options: ["RIP","OSPF","ARP","DHCP"], answer: 1 },
+  { q: "SDN stands for:", options: ["Software Defined Networking","Static Data Node","System Driven Network","Secure Data Node"], answer: 0 },
+  { q: "The 'slow start' congestion control mechanism belongs to which protocol?", options: ["UDP","TCP","ICMP","ARP"], answer: 1 },
+  { q: "Which device operates at the network layer to connect different networks?", options: ["Hub","Switch","Router","Repeater"], answer: 2 }
+];
